a6e9a4dfda1da1479c90f5b8f145ba7e70cf6762
shavavo <shavavo@google.com>
2020-07-27 20:14:08 -0400

    added to registry

--- core/jobs_registry.py
+++ core/jobs_registry.py
@@ -63,6 +63,7 @@ ONE_OFF_JOB_MANAGERS = [
     exp_jobs_one_off.ItemSelectionInteractionOneOffJob,
     exp_jobs_one_off.MathExpressionValidationOneOffJob,
     exp_jobs_one_off.MultipleChoiceInteractionOneOffJob,
+    exp_jobs_one_off.RuleSpecOneOffJob,
     exp_jobs_one_off.ViewableExplorationsAuditJob,
     feedback_jobs_one_off.FeedbackThreadCacheOneOffJob,
     opportunity_jobs_one_off.ExplorationOpportunitySummaryModelRegenerationJob,
